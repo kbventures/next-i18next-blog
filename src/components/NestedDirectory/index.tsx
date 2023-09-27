ec3a36e8d3209422d498f41f3fb6c113f238e2cc
kbventures <kmb@hotmail.com>
2023-09-27 11:09:40 -0500

    Example of multi language namespaces import

--- src/components/NestedDirectory/index.tsx
+++ src/components/NestedDirectory/index.tsx
@@ -1,13 +1,31 @@
+// import { useTranslation } from "next-i18next";
+
+// const NestedDirectory = () => {
+
+//   const { t } = useTranslation("directorynesting/other");
+//   return (
+//         <div>
+//             <p>{t("nesteddirectory")}</p>
+//         </div>
+//   );
+// };
+
+// export default NestedDirectory;
+
+
 import { useTranslation } from "next-i18next";
 
 const NestedDirectory = () => {
 
   const { t } = useTranslation("directorynesting/other");
+
+  const { t: commonT } = useTranslation("common");
+  const { t: directorynestingT } = useTranslation("directorynesting/other");
   return (
         <div>
-            <p>{t("nesteddirectory")}</p>
+            <h1>{directorynestingT("nesteddirectory")}</h1>
+            <p>{commonT("helloworld")}</p>
         </div>
   );
 };
-
 export default NestedDirectory;
